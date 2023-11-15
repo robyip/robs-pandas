e58a4b1257d94062e120a8dad2d9dbf149bfea44
robyip <robyip@gmail.com>
2023-11-15 22:19:17 +0000

    modified:   code/pandas-readcsv.py 	new file:   code/test1.csv
    test if just quoted fields worked by removing double quoted escapechar

--- code/pandas-readcsv.py
+++ code/pandas-readcsv.py
@@ -6,7 +6,8 @@ path = os.getcwd()
 print(path)
 
 
-df = pd.read_csv("code/test.csv", header=None)
+# df = pd.read_csv("code/test.csv", header=None, quotechar=") 
+df = pd.read_csv("code/test1.csv", header=None, quotechar='"') 
 
 print(df.head())
 
